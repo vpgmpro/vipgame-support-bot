c755ba93b40b35bdf2800ef895dbaaec41073530
vpgmpro <letterforus@proton.me>
2026-07-10 19:32:34 +0500

    Add debug output for BOT_TOKEN and SUPPORT_CHAT_ID
    Added debug prints for environment variables.

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import os
 
-BOT_TOKEN=os.getenv("BOT_TOKEN")
-SUPPORT_CHAT_ID=os.getenv("SUPPORT_CHAT_ID")
+print("=" * 50)
+print("DEBUG ENV")
+print("BOT_TOKEN =", os.getenv("BOT_TOKEN"))
+print("SUPPORT_CHAT_ID =", os.getenv("SUPPORT_CHAT_ID"))
+print("=" * 50)
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SUPPORT_CHAT_ID = os.getenv("SUPPORT_CHAT_ID")
