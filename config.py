c9c5d7ead41159ae1bdebd7bba7462326ffb576a
vpgmpro <letterforus@proton.me>
2026-07-10 20:46:59 +0500

    Modify bot token and admin chat ID
    Updated bot token and admin chat ID in config.

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 import os
 
 # Токен бота (можно хранить в переменных окружения)
-TOKEN = "ВАШ_ТОКЕН_ОТ_BOTFATHER"  # Замените на ваш токен
+TOKEN = "8709088661:AAF_wzJjk_djr4Cz8Zz3nuLNPHyDmOG-obc"  # Замените на ваш токен
 
 # ID администратора (узнать у @userinfobot)
-ADMIN_CHAT_ID = 123456789  # Замените на ваш ID
+ADMIN_CHAT_ID = -5177697795  # Замените на ваш ID
 
 # Путь к файлу с FAQ
 FAQ_FILE = "faq.json"
